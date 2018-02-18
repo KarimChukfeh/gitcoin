c2fa51a64155d74f2b795e71b05a868fc05bf93b
KarimChukfeh <kchuk030@uottawa.ca>
2018-02-17 22:35:23 -0500

    listen to new node

--- script.py
+++ script.py
@@ -4,7 +4,16 @@ import git
 from git import Repo
 import os
 import re
+import mysql.connector
 
+
+db_config = {
+    'user' : 'bd40b5504b9f3e',
+    'password' : '0e48730d',
+    'database' : 'heroku_c5834a6e30a6592',
+    'host' : 'us-cdbr-iron-east-05.cleardb.net',
+    'port' : '3306'
+}
 
 VERIFIED_NODES = ['https://github.com/karimchukfeh', 'https://github.com/youssefe', 'https://github.com/osfalos', 'https://github.com/sanaknaki']
 
@@ -31,9 +40,22 @@ def remote_node_exists(organization, no_forks=True):
             return True
     return False
 
+def listen_for_new_node():
+    numberOfNodes = len(VERIFIED_NODES)
+
+    db_connection = mysql.connector.connect(**db_config)
+    cursor = db_connection.cursor()
+    query = "SELECT COUNT(*) FROM information_schema.TABLES WHERE TABLE_SCHEMA=%s;"
+
+    cursor.execute(query, (db_config['database']))
+    result = cursor.fetch()
+    print result
+    db_connection.close()
+
 
 
 if __name__ == '__main__':
     print "hi"
     # clone_repo_from_random_node()
     # remote_node_exists("karimchukfeh")
+    listen_for_new_node()
