17d76584255026b89167aa1df686837fc35d9f0d
KarimChukfeh <kchuk030@uottawa.ca>
2018-02-18 01:19:27 -0500

    create new node

--- script.py
+++ script.py
@@ -15,7 +15,7 @@ db_config = {
     'port' : '3306'
 }
 
-VERIFIED_NODES = ['https://github.com/karimchukfeh', 'https://github.com/youssefe', 'https://github.com/osfalos', 'https://github.com/sanaknaki']
+VERIFIED_NODES = ['karimchukfeh', 'youssefe', 'osfalos', 'sanaknaki']
 
 def getLocalGitUser():
     return "karimchukfeh"
@@ -24,8 +24,7 @@ def clone_repo_from_random_node():
     random_node = random.choice(VERIFIED_NODES)
     if not os.path.isdir("Node"):
         os.makedirs("Node")
-        git.Git("Node").clone('https://github.com/karimchukfeh/gitcoin')
-        broadcast_to_random_nodes('NewNodeNotification', )
+        git.Git("Node").clone('https://github.com/'+ random_node + '/gitcoin.git')
 
 
 def remote_node_exists(organization, no_forks=True):
@@ -33,24 +32,22 @@ def remote_node_exists(organization, no_forks=True):
     gh = pygithub3.Github(token='3ec8183955c53ad0acfbf3fdccbe874e561b4acd')
     all_repos = gh.repos.list(user=organization).all()
     for repo in all_repos:
-        # Don't print the urls for repos that are forks.
         if no_forks and repo.fork:
             continue
         if re.split('/', repo.clone_url)[-1] == "gitcoin.git":
             return True
     return False
 
-def listen_for_new_node():
+def new_node_table_exists():
     numberOfNodes = len(VERIFIED_NODES)
-
     db_connection = mysql.connector.connect(**db_config)
     cursor = db_connection.cursor()
     query = "SELECT * FROM information_schema.tables WHERE TABLE_TYPE = 'BASE TABLE'"
-
     cursor.execute(query, (db_config['database']))
     result = cursor.fetchall()
+    for table in result:
+
     db_connection.close()
-    print len(result)
 
 
 def create_git_repo_init():
@@ -71,10 +68,22 @@ def create_git_repo_init():
     another_url = 'https://github.com/osfalos/gitcoin.git'
     remote = repo.create_remote('gitcoin', url=another_url)
     remote.push()
+    create_new_node_table(username)
     return True
+
+def create_new_node_table(username):
+    db_connection = mysql.connector.connect(**db_config)
+    cursor = db_connection.cursor()
+    tableName = username.encode('utf8', 'replace')
+    query = "CREATE TABLE " + username + " (`transaction_id` VARCHAR(100) NOT NULL, `sender` VARCHAR(100) NULL, `reciever` VARCHAR(100) NULL, `amount` INT(100) NULL, `status` VARCHAR(45) NULL DEFAULT 'broadcasted', PRIMARY KEY (`transaction_id`));"
+    cursor.execute(query, (tableName))
+    db_connection.close()
+    return True
+
 
 if __name__ == '__main__':
     print "hi"
     # clone_repo_from_random_node()
     # remote_node_exists("karimchukfeh")
-    listen_for_new_node()
+    new_node_table_exists()
+    # create_new_node_table("sanaknaki")
