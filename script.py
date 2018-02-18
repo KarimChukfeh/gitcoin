2b4f6bfdbdda2c7dfb0d22a199e3f3d8d0ce9c4c
osfalos <chahdoura.os1@gmail.com>
2018-02-18 02:37:20 -0500

    add verification part

--- script.py
+++ script.py
@@ -17,8 +17,10 @@ db_config = {
 
 VERIFIED_NODES = ['https://github.com/karimchukfeh', 'https://github.com/youssefe', 'https://github.com/osfalos', 'https://github.com/sanaknaki']
 
-def getLocalGitUser():
-    return "karimchukfeh"
+def get_local_git_user():
+    repo = Repo()
+    conf_reader = repo.config_reader()
+    return conf_reader.get_value('user', 'name')
 
 def clone_repo_from_random_node():
     random_node = random.choice(VERIFIED_NODES)
@@ -55,26 +57,89 @@ def listen_for_new_node():
 
 def create_git_repo_init():
     gh = None
-    username = raw_input()
-    password = raw_input()
-    auth = dict(login=username, password=password)
-    gh = Github(**auth)
+    #print "enter your username"
+    #username = raw_input()
+    #print "enter your password"
+    #password = raw_input()
+    #auth = dict(login=username, password=password)
+    username = get_local_git_user()
+    print(username)
+    gh = pygithub3.Github(token="793a78550d17324ec385ec16d8b76ec6575b83c4")
     repo_name = 'gitcoin'
-    octocat = gh.users.get()
-    print octocat
-    get_user = gh.users.get()
-    gh.repos.create(dict(name='gitcoin', description='desc'))
+    gh.repos.create(dict(name=repo_name, description='desc'))
     #repos = gh.create_repo(repo_name)
     cloneUrl='https://github.com/karimchukfeh/gitcoin.git'
     localRepopath = 'clonetest/'
     repo = Repo.clone_from(cloneUrl, localRepopath)
-    another_url = 'https://github.com/osfalos/gitcoin.git'
-    remote = repo.create_remote('gitcoin', url=another_url)
+    another_url = 'https://github.com/'+username+'/gitcoin.git'
+    remote = repo.create_remote(repo_name, url=another_url)
     remote.push()
     return True
 
-if __name__ == '__main__':
-    print "hi"
+def transaction_verification():
+    resp = null
+    while(resp == null):
+        resp = get_new_transaction_query()
+    if(verify_sender(resp[0],resp[2]) and verify_receiver(resp[1])):
+        #call broadcast
+        return True
+    else:
+        return False
+
+def clone_repo(clone_username,type):
+    dir_name = "clone_"+type+"_"+clone_username
+    if not os.path.isdir(dir_name):
+        os.makedirs(dir_name)
+    cloneUrl="https://github.com/"+str(clone_username)+"/gitcoin.git"
+    localRepopath = dir_name+"/"
+    repo = Repo.clone_from(cloneUrl, localRepopath)
+    return repo
+
+def verify_sender(sender, amount):
+    repo_clone = clone_repo(sender,"sender_verif")
+    repo = Repo()
+    if((verif_commit_clone(repo,repo_clone))):
+        return False
+    return True
+
+def verify_receiver(reciever):
+    repo_clone =  clone_repo(reciever,"receiver_verif")
+    repo = Repo()
+    if((verif_commit_clone(repo,repo_clone))):
+        return False
+    return True
+
+def verif_commit_clone(repo,repo_clone):
+    commits_repo = list(repo.iter_commits('master'))
+    print(len(commits_repo))
+    commits_cloned_repo = list(repo_clone.iter_commits('master'))
+    print(len(commits_cloned_repo))
+    for commit in commits_repo:
+        for commit_compare in commits_cloned_repo:
+            if(not(commit.hexsha  == commit_compare.hexsha)):
+                return False
+    return True
+
+def get_new_transaction_query():
+    db_connection = mysql.connector.connect(**db_config)
+    cursor = db_connection.cursor()
+    query = "SELECT * FROM "+ db_config['database'] + "." + get_local_git_user()
+    cursor.execute(query, (db_config['database']))
+    result = cursor.fetchall()
+    db_connection.close()
+    # TODO exec query get result
+    last = result[-1]
+    if last[-1] == 'broadcasted':
+        db_connection.close()
+        return last
+    else:
+        db_connection.close()
+        return null
+
+
     # clone_repo_from_random_node()
     # remote_node_exists("karimchukfeh")
-    listen_for_new_node()
+#listen_for_new_node()
+#create_git_repo_init()
+print(verify_sender("osfalos","25"))
+print(verify_receiver("osfalos"))
